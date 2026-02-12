4cb841b9a73c213f7fd71f0cabf8b9a326b9da37
Ricky <vianeyricky@gmail.com>
2026-02-12 22:18:10 +0700

    fix footer dan security

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,26 @@
 import type { NextConfig } from "next";
 
+
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data: https://images.unsplash.com https://www.transparenttextures.com;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`;
+
 const nextConfig: NextConfig = {
 
   typescript: {
     ignoreBuildErrors: true,
   },
   
-
   images: {
     remotePatterns: [
       {
@@ -18,6 +32,45 @@ const nextConfig: NextConfig = {
         hostname: 'www.transparenttextures.com',
       },
     ],
+  },
+
+
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, '')
+          }
+        ],
+      },
+    ];
   },
 };
 
