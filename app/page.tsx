fd97eb0351ae7ee5659e694c01823a885330dca3
Ricky <vianeyricky@gmail.com>
2026-02-12 22:00:03 +0700

    fix header image

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         {/* BACKGROUND IMAGE FIX (PASTI MUNCUL) */}
         <div className="absolute inset-0 z-0">
           <Image 
-            src="https://images.unsplash.com/photo-1622036672492-67a5a36244a6?auto=format&fit=crop&q=80" 
+            src="https://images.unsplash.com/photo-1576473511969-6ea4136f27c9?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
             alt="Background Sawit" 
             fill
             className="object-cover"
